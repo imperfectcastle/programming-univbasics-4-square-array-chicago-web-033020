af61543788bc1ccef63acad2f3ddaac0fd9f0e69
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-10 12:53:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
+  new_array = []
   counter = 0
-
-  while array[counter] do
-    puts array[counter]
-    counter.sqrt
+  while counter < array.length
+    new_array.push(array[counter].sqrt)
     counter += 1
   end
+  return new_array
 end
